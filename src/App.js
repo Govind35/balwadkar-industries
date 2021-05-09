3621af32f29e302d19431dc32a4a1e911b8356f5
Govind kalsa <govind.kalsa@vrlabtech.com>
2021-05-09 19:21:24 +0530

    created new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from '../src/component/Navbar'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
